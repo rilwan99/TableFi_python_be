8e906c6d9dd86db3b950b12f31047798c1b5fda3
Mocha <wongtracy793@gmail.com>
2022-08-16 13:31:58 +0800

    make iterable for onchain as long as txn can be called from api

--- flaskr/call_solscan.py
+++ flaskr/call_solscan.py
@@ -3,7 +3,18 @@ import requests
 import json
 import pandas as pd
 import numpy as np
-response_API = requests.get('https://public-api.solscan.io/account/splTransfers?account=Ck1b1oTDXvDDWmci5j2XCCtRQ9QmAByeGFqJLhqzoXqN&fromTime=1640995200')
+pd.options.mode.chained_assignment = None
+
+time = '1640995200' #using 1 april 2022
+walletPublicKey = 'Ck1b1oTDXvDDWmci5j2XCCtRQ9QmAByeGFqJLhqzoXqN'
+url = 'https://public-api.solscan.io/account/splTransfers?account='
+
+requestString = url + walletPublicKey + '&fromTime=' + time
+print(requestString)
+
+response_API = requests.get(requestString) #takes last 10 txn per public API
+#response_API = requests.get('https://public-api.solscan.io/account/splTransfers?account=Ck1b1oTDXvDDWmci5j2XCCtRQ9QmAByeGFqJLhqzoXqN&fromTime=1640995200')
+stablecoins = ['USDC', 'USDT']
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -11,47 +22,24 @@ data = response_API.text
 parse_json = json.loads(data)
 
 a = pd.DataFrame(parse_json['data'])
-print(a)
+
+#take max amt to avg out price
 temp = a[['symbol', 'preBalance','postBalance', 'changeAmount','fee', 'decimals']]
-# totalFees = temp['fee'].sum()
+totalFees = temp['fee'].sum()
 
-# temp[['changeAmount','fee', 'preBalance', 'postBalance']] = temp[['changeAmount','fee', 'preBalance', 'postBalance']].astype(float)
-# temp['finalAmt']=temp['changeAmount']/pow(10, temp['decimals'])
-# temp['maxAmt']=temp['finalAmt']
+temp[['changeAmount','fee', 'preBalance', 'postBalance']] = temp[['changeAmount','fee', 'preBalance', 'postBalance']].astype(float)
+temp['finalAmt']=temp['changeAmount']/pow(10, temp['decimals'])
+temp['maxAmt']=temp['finalAmt']
+walletDataNoUSD = temp[~temp['symbol'].isin(stablecoins)]  #remove stablecoins
 
-# temp_2 = temp.groupby('symbol',as_index=False).agg({'changeAmount':'sum','fee':'sum', 'preBalance':'sum', 'postBalance':'sum', 'decimals':'first', 'finalAmt':'sum', 'maxAmt':'max'})
-# print(temp_2)
-# avg_price = sum(temp_2.loc[temp_2['symbol']=='USDC']['maxAmt'], temp_2.loc[temp_2['symbol']=='USDT']['maxAmt'])
-# print(temp_2[temp_2['symbol']=='GST']['maxAmt'])
-# returnDict = {temp_2['symbol'][i]:  "-" for i in range(len(temp_2['symbol']))}
-# #validDict = {temp_2['symbol'][0]:0}
-# #print(temp_2['symbol'][0])
-# b = temp_2.loc[temp_2['symbol']=='GST']['maxAmt']
-# for i in b:
-#     c = i
-# for i in avg_price:
-#     a = (i/c)
-# validDict = {temp_2['symbol'][0]:a}
-# print(validDict)
-# returnDict = {temp_2['symbol'][i]:  "-" for i in range(len(temp_2['symbol']))}
-# for k, v in returnDict.items():
-#         if k in validDict:
-#             returnDict[k] = validDict.get(k)
-# print(returnDict) 
-#return returnDict
-#print(temp_2[temp_2['symbol']=='USDT'].maxAmt.add(temp_2[temp_2['symbol']=='USDC'].maxAmt))
-#/temp_2[temp_2['symbol']=='GST']['maxAmt']
-
-# #returnDict = {}
-# returnDict = {temp_2['symbol'][i]:  "-" for i in range(len(temp_2['symbol']))}
-# print(returnDict)
-# print(temp_2[temp_2['symbol']=='USDC']['postBalance'])
-#returnlist = []
-#for i in range(0, len(temp_2)):
-    # if temp_2.iloc[i]['changeAmount'] > 0:
-    #     returnlist.append(temp_2[i]['changeAmount'])
-    #     avg_price = ''
-# print(a.groupby('symbol').count())
-# a['symbol','slot','']
-# import sys
-# print(sys.maxsize)
+temp_2 = temp.groupby('symbol',as_index=False).agg({'changeAmount':'sum','fee':'sum', 'preBalance':'sum', 'postBalance':'sum', 'decimals':'first', 'finalAmt':'sum', 'maxAmt':'max'})
+stablecoinsAmt = temp_2.loc[temp_2['symbol'].isin(stablecoins)]['maxAmt'].sum()
+returnDict = {temp_2['symbol'][i]:  "-" for i in range(len(temp_2['symbol']))}
+walletTokenMaxAmt = temp_2[temp_2['symbol'].isin(walletDataNoUSD['symbol'])]['maxAmt']
+getUniqueSymbol = walletDataNoUSD['symbol'].unique()
+avgEntryPrice = []
+for idx, i in enumerate(getUniqueSymbol):
+    avgEntryPrice.append(stablecoinsAmt/walletTokenMaxAmt[idx])
+resultDict = {getUniqueSymbol[i]: avgEntryPrice[i] for i in range(len(getUniqueSymbol))}
+print(resultDict)
+#return resultDict
